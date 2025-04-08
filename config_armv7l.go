5cec50a7c791664e2c5abcea14ba07ded8657e78
Morris Kelly <morris.kelly@synapt.ec>
2025-04-08 13:30:19 +0100

    remove -v flag

--- config_armv7l.go
+++ config_armv7l.go
@@ -3,5 +3,5 @@
 package iec61850
 
 // #cgo CFLAGS: -I./libiec61850/inc/hal/inc -I./libiec61850/inc/common/inc -I./libiec61850/inc/goose -I./libiec61850/inc/iec61850/inc -I./libiec61850/inc/iec61850/inc_private -I./libiec61850/inc/logging -I./libiec61850/inc/mms/inc -I./libiec61850/inc/mms/inc_private -I./libiec61850/inc/mms/iso_mms/asn1c
-// #cgo LDFLAGS: -static-libgcc -v -static-libstdc++ -L./libiec61850/lib/linux_armv7l -liec61850 -lpthread
+// #cgo LDFLAGS: -static-libgcc -static-libstdc++ -L./libiec61850/lib/linux_armv7l -liec61850 -lpthread
 import "C"
