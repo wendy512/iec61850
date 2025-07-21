0009dd45946db2f32f1c4765beab8606f727c54f
wzh <smts@dev.dev>
2025-07-21 11:12:18 +0800

    添加缺失的类型声明

--- tls_config.go
+++ tls_config.go
@@ -11,7 +11,7 @@ type TLSConfigVersion int
 
 const (
 	TLS_VERSION_NOT_SELECTED TLSConfigVersion = 0
-	TLS_VERSION_SSL_3_0                       = 3
+	TLS_VERSION_SSL_3_0      TLSConfigVersion = 3
 	TLS_VERSION_TLS_1_0                       = 4
 	TLS_VERSION_TLS_1_1                       = 5
 	TLS_VERSION_TLS_1_2                       = 6
