bfc57cd610f6dfc5dff263649015c86cc0a826ba
wzh <3783613361@qq.com>
2025-07-25 11:13:10 +0800

    features: 添加 sv 功能的相关头文件和配置

--- config_linux64.go
+++ config_linux64.go
@@ -2,6 +2,6 @@
 
 package iec61850
 
-// #cgo CFLAGS: -I./libiec61850/inc/hal/inc -I./libiec61850/inc/common/inc -I./libiec61850/inc/goose -I./libiec61850/inc/iec61850/inc -I./libiec61850/inc/iec61850/inc_private -I./libiec61850/inc/logging -I./libiec61850/inc/mms/inc -I./libiec61850/inc/mms/inc_private -I./libiec61850/inc/mms/iso_mms/asn1c
+// #cgo CFLAGS: -I./libiec61850/inc/hal/inc -I./libiec61850/inc/common/inc -I./libiec61850/inc/goose -I./libiec61850/inc/sampled_values -I./libiec61850/inc/iec61850/inc -I./libiec61850/inc/iec61850/inc_private -I./libiec61850/inc/logging -I./libiec61850/inc/mms/inc -I./libiec61850/inc/mms/inc_private -I./libiec61850/inc/mms/iso_mms/asn1c
 // #cgo LDFLAGS: -static-libgcc -static-libstdc++ -L./libiec61850/lib/linux64 -liec61850 -lpthread
 import "C"
