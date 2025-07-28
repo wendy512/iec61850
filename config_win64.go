9f59350f52f2bf2219e05d55612821755d7e0a60
wzh <3783613361@qq.com>
2025-07-28 11:09:16 +0800

    chore: 添加 npcap 依赖, 重新编译 libiec61850, 以支持 windows amd64 下的开发测试

--- config_win64.go
+++ config_win64.go
@@ -3,5 +3,5 @@
 package iec61850
 
 // #cgo CFLAGS: -I./libiec61850/inc/hal/inc -I./libiec61850/inc/common/inc -I./libiec61850/inc/goose -I./libiec61850/inc/sampled_values -I./libiec61850/inc/iec61850/inc -I./libiec61850/inc/iec61850/inc_private -I./libiec61850/inc/logging -I./libiec61850/inc/mms/inc -I./libiec61850/inc/mms/inc_private -I./libiec61850/inc/mms/iso_mms/asn1c
-// #cgo LDFLAGS: -static-libgcc -static-libstdc++ -L${SRCDIR}/libiec61850/lib/win64 -liec61850 -lhal -lws2_32
+// #cgo LDFLAGS: -static-libgcc -static-libstdc++ -L${SRCDIR}/libiec61850/lib/win64 -liec61850 -lhal -lwpcap -lpacket -lws2_32 -liphlpapi
 import "C"
